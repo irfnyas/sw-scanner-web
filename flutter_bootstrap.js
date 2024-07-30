8bf2f380cc10ceaf9320b2989e2c252e30559b75
Irfan Yas <irfanyasiras@gmail.com>
2024-07-30 11:51:21 +0700

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2370475765"
+    serviceWorkerVersion: "1501829548"
   }
 });
