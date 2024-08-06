e253c1b7d2d248efa1fcd481408eda70905132a9
Irfan Yas <irfanyasiras@gmail.com>
2024-08-06 16:09:24 +0700

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -385,16 +385,3 @@ _flutter.loader.load({
     serviceWorkerVersion: "783858506",
   },
 });
-
-const loading = document.createElement("div");
-document.body.appendChild(loading);
-loading.textContent = "Loading Entrypoint...";
-_flutter.loader.load({
-  onEntrypointLoaded: async function (engineInitializer) {
-    loading.textContent = "Initializing engine...";
-    const appRunner = await engineInitializer.initializeEngine();
-
-    loading.textContent = "Running app...";
-    await appRunner.runApp();
-  },
-});
